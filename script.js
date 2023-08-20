1ba284aa1b6670df56bd4c249de50d05534a1d22
Duc Manh <ducmanhdut248@gmail.com>
2023-08-20 22:56:49 +0700

    fix responsive auth on mobile

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const $$ = document.querySelectorAll.bind(document);
 const toggleMenu = $(".navbar__toggle-menu");
 const overlay = $(".overlay");
 const closeBtn = $(".navbar__close-btn");
-const navItems = $$(".nav-item");
-console.log(overlay);
-const nav = $(".nav");
+const navItems = $$(".nav-item-mobile");
+const navBar = $(".navbar");
+const drawerMenu = $('.drawer-menu');
+const navOnPC = $(".nav");
+const navOnMobile = $('.nav-mobile')
 
 toggleMenu.onclick = () => {
     overlay.classList.toggle("open");
     toggleMenu.style.display = "none";
     closeBtn.style.display = "inline-block";
+    drawerMenu.style.display = "block";
 };
 overlay.onclick = () => {
     overlay.classList.remove("open");
@@ -25,19 +28,17 @@ closeBtn.onclick = () => {
     toggleMenu.style.display = "inline-block";
     closeBtn.style.display = "none";
 }
-
+// console.log(body.clientWidth);
 navItems.forEach((navItem) => {
+    console.log(navItem)
+
     navItem.onclick = () => {
+        console.log(navItem)
         overlay.classList.remove("open");
         toggleMenu.style.display = "inline-block";
         closeBtn.style.display = "none";
-
-        // closeBtn.style.opacity = "0";
-        // closeBtn.style.visivility = "hidden";
-        // closeBtn.style.transition = "0.5 ease";
-
-
-
+        drawerMenu.style.display = "none";
     }
 
 })
+navOnMobile.innerHTML = navOnPC.innerHTML;
