9f0d299c03a557c7f7683655a614eb5328ef40c9
Duc Manh <ducmanhdut248@gmail.com>
2023-08-25 14:57:05 +0700

    Optimize code

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const navBar = $(".navbar");
 const drawerMenu = $('.drawer-menu');
 const navOnPC = $(".nav");
 const navOnMobile = $('.nav-mobile')
-
+console.log(navItems)
 toggleMenu.onclick = () => {
     overlay.classList.toggle("open");
     toggleMenu.style.display = "none";
@@ -29,15 +29,16 @@ closeBtn.onclick = () => {
     closeBtn.style.display = "none";
 }
 // console.log(body.clientWidth);
+navOnMobile.innerHTML = navOnPC.innerHTML;
+
 navItems.forEach((navItem) => {
     // console.log(window.innerWidth)
-
-    navItem.onclick = () => {
-        console.log(navItem)
+    console.log(navItem)
+    navItem.addEventListener('click', () => {
         overlay.classList.remove("open");
         closeBtn.style.display = "none";
         drawerMenu.style.display = "none";
-    }
+    })
+
 
 })
-navOnMobile.innerHTML = navOnPC.innerHTML;
